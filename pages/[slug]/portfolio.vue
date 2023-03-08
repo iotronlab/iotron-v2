0412403e53a6441dddb628f3b48622fd0c759dec
Prathamesh Pawar <cm.b26.prathameshpawar@gmail.com>
2023-03-08 18:20:24 +0530

    portfolio-slug

--- pages/[slug]/portfolio.vue
+++ pages/[slug]/portfolio.vue
@@ -6,8 +6,95 @@
 
     <section v-else>
       <Breadcrumb :breadcrumb-items="breadcrumbItems" />
+          <v-img
+            v-if="portfolio.image != null"
+            :aspect-ratio="16 / 9"
+            width="100%"
+            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+          ></v-img>
+      <div class="flex flex-col justify-center items-center">
+            <div class="relative flex flex-col items-center rounded-[20px]  max-w-[95%] mx-auto bg-slate-400 bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:bg-slate-500 dark:text-white dark:!shadow-none p-3">
+                  <div>
+                    <h2 class="font-weight-bold text-center">Name</h2>
+                    <h3 class="subtitle-1 h-6 text-lg">{{ portfolio.name }}</h3>
+                  </div>
+                <div class="mt-2 mb-8 w-full">
+                    <h4 class="px-2 text-xl font-bold text-navy-700 dark:text-white">
+                    Client Brief
+                    </h4>
+                    <p class="mt-2 px-2 text-base text-gray-300">
+                    {{ portfolio.client_brief }}
+                    </p>
+                </div> 
+                <div class="mt-2 mb-8 w-full">
+                    <h4 class="px-2 text-xl font-bold text-navy-700 dark:text-white">
+                    Project Description
+                    </h4>
+                    <p class="mt-2 px-2 text-base text-gray-300">
+                    {{ portfolio.project_description }}
+                    </p>
+                </div> 
+                <div class="grid grid-cols-2 gap-4 px-2 w-full">
+                    <div v-if="portfolio.client_location" class="flex flex-col items-start justify-center rounded-2xl bg-slate-400 bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                      <p class="text-sm dark:text-white ">Client Location</p>
+                      <p class="text-base font-medium text-navy-700 text-gray-600">
+                          {{portfolio.client_location}}
+                      </p>
+                    </div>
 
-      <v-sheet
+                    <div v-if="portfolio.tools != null" class="flex flex-col items-start justify-center rounded-2xl bg-slate-400 bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                      <p class="text-sm dark:text-white ">Tools</p>
+                      <p class="text-base font-medium text-navy-700 text-gray-600">
+                          <v-chip
+                            v-for="(tool, i) in portfolio.tools"
+                            :key="i"
+                            outlined
+                            class="ma-1"
+                          >
+                            {{ tool }}</v-chip
+                          >
+                      </p>
+                    </div>
+                    <div v-if="portfolio.services != null" class="flex flex-col items-start justify-center rounded-2xl bg-slate-400 bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                      <p class="text-sm dark:text-white ">Services</p>
+                      <p class="text-base font-medium text-navy-700 text-gray-600">
+                          <v-chip
+                            v-for="(service, i) in portfolio.services"
+                            :key="i"
+                            outlined
+                            class="ma-1"
+                            >{{ service }}
+                          </v-chip>
+                      </p>
+                    </div>
+                    <div v-if="portfolio.external_url != null" class="flex flex-col items-start justify-center rounded-2xl bg-slate-400 bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                      <p class="text-sm dark:text-white ">External Urls</p>
+                      <p class="text-base font-medium text-navy-700 text-gray-600">
+                        <v-btn
+                          v-for="(url, i) in portfolio.external_url"
+                          :key="i"
+                          class="ma-1 btn-creator"
+                          :href="url.url"
+                          target="_href"
+                          rounded
+                          dark
+                          >{{ url.site }}</v-btn
+                        >
+                         <v-col class="col-12 pl-0">
+                          <v-btn class="btn-agency" outlined @click="share"
+                            ><v-icon left icon="$share"></v-icon> Share</v-btn
+                          >
+                        </v-col>
+                      </p>
+                    </div>
+                    <v-col class="col-12 col-lg-8">
+                      <PostImages :images="portfolio.images"
+                    /></v-col>
+                </div>
+            </div>  
+            <p class="font-normal text-navy-700 mt-20 mx-auto w-max">Profile Card component from <a href="https://horizon-ui.com?ref=tailwindcomponents.com" target="_blank" class="text-brand-500 font-bold">Horizon UI Tailwind React</a></p>  
+        </div>
+      <!-- <v-sheet
         color="sucess"
         height="100%"
         width="100%"
@@ -23,7 +110,7 @@
           ></v-img>
 
           <v-card-title>
-            <h1 class="text-h4 text-center">
+            <h1 class="text-h4 text-center my-3 mb-4">
               Case Study - {{ portfolio.name }}
             </h1>
           </v-card-title>
@@ -113,7 +200,7 @@
         <v-col class="col-12 col-lg-8">
           <PostImages :images="portfolio.images"
         /></v-col>
-      </v-sheet>
+      </v-sheet> -->
     </section>
   </v-container>
     
